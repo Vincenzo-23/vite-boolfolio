1479b49737790e6b872ab723b158fecc42abd0b9
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-06-13 15:14:18 +0200

    Installed vue router. Added the Home, Portfolio and Contact pages inside the pages folder. Added the routes to the file main.js

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
-  <AppMain />
+  <!-- <AppMain /> -->
+
+  <header class="d-flex align-items-center p-4">
+    <h1>Vite Boolfolio</h1>
+    <nav class="d-flex gap-3 ms-auto">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/portfolio">Portfolio</RouterLink>
+      <RouterLink to="/contact">Contatti</RouterLink>
+    </nav>
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+
+
 </template>
 
 
--- src/main.js
+++ src/main.js
@@ -2,4 +2,22 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createWebHistory, createRouter } from 'vue-router'
+
+import Home from './pages/Home.vue'
+import Portfolio from './pages/Portfolio.vue'
+import Contact from './pages/Contact.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/portfolio', component: Portfolio },
+  { path: '/contact', component: Contact },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+
+createApp(App).use(router).mount('#app')
